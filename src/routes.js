90da6558f8e7eb427e6f5bd7a41cfce9c6955b5b
vinisted <viniciusaraujo.145@outlook.com>
2024-05-30 17:00:58 -0300

    Teste de commit 2 vinicius

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,7 @@
 
 */
 // @material-ui/icons
+
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
